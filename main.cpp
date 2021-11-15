d79f5297546509ccb361bdbdd747dc95bfe217d3
SIMOES <Vincent.SIMOES@wright.com>
2021-11-15 15:59:46 +0100

    Only ITK for testing

--- main.cpp
+++ main.cpp
@@ -6,24 +6,10 @@
 /*---- ITK Includes ----*/
 #include <itkImage.h>
 
-/*---- QT Includes ----*/
-#include <qdebug.h>
-
-/*---- VTK Includes ----*/
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
-
 int main(int p_argc, char* p_argv[])
 {
-	qDebug() << "Starting test ...";
-	
 	itk::Image<unsigned char, 2>::Pointer image = itk::Image<unsigned char, 2>::New();
-	qDebug() << "ITK OK";
-
-	vtkSmartPointer<vtkPolyData> mesh = vtkSmartPointer<vtkPolyData>::New();
-	qDebug() << "VTK OK";
-
-	qDebug() << "QT OK";
+	std::cout << "ITK OK";
 
 	return 0;
 } 

f2a29508d8eb7c7749381942dda54c6add01205b
SIMOES <vincent.simoes@stryker.com>
2022-10-03 17:52:42 +0200

    Update VTK

--- FindExternalLibs.cmake
+++ FindExternalLibs.cmake
@@ -3,25 +3,19 @@
 # ------------------------------------------------------------------------------
 # Set External libraries path
 # ------------------------------------------------------------------------------
-set( EXTERNAL_LIBS_DIR "D:/ExternalLibs" CACHE STRING "External libs Dir" )
+set( EXTERNAL_LIBS_DIR "C:/ExternalLibs" CACHE STRING "External libs Dir" )
 
 # ------------------------------------------------------------------------------
 # set External libraries version
 # ------------------------------------------------------------------------------
-set( ITK_VERSION 	"5.1.2" 	CACHE STRING "ITK Version" )
-set( VTK_VERSION 	"9.0.1" 	CACHE STRING "VTK Version" )
+set( ITK_VERSION 	"5.2.1" 	CACHE STRING "ITK Version" )
+set( VTK_VERSION 	"9.1.0" 	CACHE STRING "VTK Version" )
 
 # ------------------------------------------------------------------------------
 # set External libraries directories
 # ------------------------------------------------------------------------------
-set( ITK_DIR2 	"${EXTERNAL_LIBS_DIR}/ITK" 		CACHE STRING "ITK Dir" )
-set( VTK_DIR2 	"${EXTERNAL_LIBS_DIR}/VTK" 		CACHE STRING "VTK Dir" )
-
-
-# ------------------------------------------------------------------------------
-# GLOBAL Functions
-# ------------------------------------------------------------------------------
-find_package ( GlobalFunctions )
+set( ITK_DIR2 	"${EXTERNAL_LIBS_DIR}/itk" 		CACHE STRING "ITK Dir" )
+set( VTK_DIR2 	"${EXTERNAL_LIBS_DIR}/vtk" 		CACHE STRING "VTK Dir" )
 
 
 # ------------------------------------------------------------------------------
@@ -54,7 +48,7 @@ endif()
 if( NOT EXISTS ${VTK_DIR2} )
     message( SEND_ERROR "Cant't find VTK in ${EXTERNAL_LIBS_DIR}" )
 else()
-	set( VTK_DIR "${VTK_DIR2}/lib/cmake/vtk-9.0")
+	set( VTK_DIR "${VTK_DIR2}/lib/cmake/vtk-9.1")
 	find_package( VTK REQUIRED )
 	include( ${VTK_USE_FILE} )	
 endif()

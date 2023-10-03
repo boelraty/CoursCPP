7d3f1786571c2ac9ae99309aa703bb0856723c93
SIMOES <vincent.simoes@stryker.com>
2023-10-03 17:22:18 +0200

    Init Ex1 ITK

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Test App
+/*!	\brief	Exemple1
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
-#include <itkImage.h>
 
-/*---- VTK Includes ----*/
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
+
 
 int main(int p_argc, char* p_argv[])
 {
-	std::cout << "Starting test ..." << std::endl;
-	
-	itk::Image<unsigned char, 2>::Pointer image = itk::Image<unsigned char, 2>::New();
-	std::cout << "ITK OK" << std::endl;
+	//Typedef to define a image type
+	typedef itk::Image<unsigned char, 2> UCharImageType;
 
-	vtkSmartPointer<vtkPolyData> mesh = vtkSmartPointer<vtkPolyData>::New();
-	std::cout << "VTK OK" << std::endl;
+	//Create image
 
+
+	//Define starting point (0, 0)
+	UCharImageType::IndexType startIndex;
+
+	//Define image size
+	UCharImageType::SizeType size;
+
+	//Define image region
+
+	//Set the region to the image
+	image->SetRegions(region);
+
+	//Set image origin (where it starts in a 2D/3D referentiel)
+	UCharImageType::PointType origin;
+
+
+	//Set image spacing
+	UCharImageType::SpacingType spacing;
+
+
+	//Allocate the image in memory
+
+	//Check image
+	std::cout << "Dimension X:" << image->GetLargestPossibleRegion().GetSize()[0] << std::endl;
+	//....
 	return 0;
 } 

af8bd14f0a928f602b5adf8b3521d4fcadc4fe47
SIMOES <vincent.simoes@stryker.com>
2023-10-06 16:51:26 +0200

    Correct Ex6 ITK

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
 #include <itkGDCMImageIO.h>
+#include <itkBinaryThresholdImageFilter.h>
 
 /*---- STL Includes ----*/
 #include <string>
@@ -15,51 +16,43 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	//Typedef to define a image type (refer to exercice 1 and replace unsigned char with signed short)
-	typedef itk::Image<short, 2> ShortImageType;
+	try {
+		//Typedef to define a image type (refer to exercice 1 and replace unsigned char with signed short)
+		typedef itk::Image<short, 2> ShortImageType;
 
-	//Create the GDCM image IO
-	itk::GDCMImageIO::Pointer gdcmObject = itk::GDCMImageIO::New();
+		//Create the GDCM image IO
+		itk::GDCMImageIO::Pointer gdcmObject = itk::GDCMImageIO::New();
 
-	//Create the reader to read an image
-	itk::ImageFileReader<ShortImageType>::Pointer reader 
-		= itk::ImageFileReader<ShortImageType>::New();
-	reader->SetFileName("C:/Users/vsimoes/Downloads/ImageDICOM");
-	reader->SetImageIO(gdcmObject);
-	reader->Update();
+		//Create the reader to read an image
+		itk::ImageFileReader<ShortImageType>::Pointer reader
+			= itk::ImageFileReader<ShortImageType>::New();
+		reader->SetFileName("C:/Users/vsimoes/Downloads/ImageDICOM");
+		reader->SetImageIO(gdcmObject);
+		reader->Update();
+		
+		//START EX6
+		//Create filter to apply threshold on image
+		typedef itk::Image<unsigned char, 2> UCharImageType;
+		typedef itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType> ThresholderType;
+		ThresholderType::Pointer thresholder = ThresholderType::New();
+		thresholder->SetInput(reader->GetOutput());
+		thresholder->SetLowerThreshold(150);
+		thresholder->SetUpperThreshold(3000);
+		thresholder->SetInsideValue(255);
+		thresholder->SetOutsideValue(0);
+		thresholder->Update();
 
-	std::cout << "Dimensions:" << reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0] << std::endl;
-	std::cout << "Spacing:" << reader->GetOutput()->GetSpacing()[0] << std::endl;
-	
-	//Check information from the GDCM ImageIO
-	char* patientName = new char[50];
-	gdcmObject->GetPatientName(patientName);
-	char* birthdate = new char[50];
-	gdcmObject->GetPatientDOB(birthdate);
+		//Write the binary image
+		itk::ImageFileWriter<UCharImageType>::Pointer writer
+			= itk::ImageFileWriter<UCharImageType>::New();
+		writer->SetFileName("C:/image.png");
+		writer->SetInput(thresholder->GetOutput());
+		writer->Update();
 
-	std::string modality;
-	gdcmObject->GetValueFromTag(std::string("0008|0060"), modality);
-	std::string seriesDate;
-	gdcmObject->GetValueFromTag(std::string("0008|0021"), seriesDate);
-
-
-	std::cout << "Patient name:" << patientName << std::endl;
-	std::cout << "Patient birthdate:" << birthdate << std::endl;
-	std::cout << "Modality:" << modality << std::endl;
-	std::cout << "SeriesDate:" << seriesDate << std::endl;
-
-	delete [] patientName;
-
-	//START EX6
-	//Create filter to apply threshold on image
-	//itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType>::Pointer thresholder =
-	//Use reader->GetOutput() as input of thresholder
-
-
-
-	//Write the binary image
-	itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
-	//Refer to ex 3 and use output of thresholder
-
+	}
+	catch (itk::ExceptionObject& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
 	return 0;
 }

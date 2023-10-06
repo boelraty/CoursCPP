0729d47efe3cf36ff835988faaeb0bd586b51641
SIMOES <vincent.simoes@stryker.com>
2023-10-06 15:46:00 +0200

    Correct Ex5 ITK

--- main.cpp
+++ main.cpp
@@ -16,36 +16,39 @@
 int main(int p_argc, char* p_argv[])
 {
 	//Typedef to define a image type (refer to exercice 1 and replace unsigned char with signed short)
+	typedef itk::Image<short, 2> ShortImageType;
 
 	//Create the GDCM image IO
+	itk::GDCMImageIO::Pointer gdcmObject = itk::GDCMImageIO::New();
 
 	//Create the reader to read an image
-	itk::ImageFileReader<ShortImageType>::Pointer reader = itk::ImageFileReader<ShortImageType>::New();
+	itk::ImageFileReader<ShortImageType>::Pointer reader 
+		= itk::ImageFileReader<ShortImageType>::New();
+	reader->SetFileName("C:/Users/vsimoes/Downloads/ImageDICOM");
+	reader->SetImageIO(gdcmObject);
+	reader->Update();
 
-
-	//Write dimensions and spacing
-	//std::cout << "Dimensions X :" << reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
-
-
+	std::cout << "Dimensions:" << reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0] << std::endl;
+	std::cout << "Spacing:" << reader->GetOutput()->GetSpacing()[0] << std::endl;
+	
 	//Check information from the GDCM ImageIO
-	char* patientname = new char;
-	char* patientbirthdate = new char;
-	char* patientid = new char;
-	char* studydate = new char;
+	char* patientName = new char[50];
+	gdcmObject->GetPatientName(patientName);
+	char* birthdate = new char[50];
+	gdcmObject->GetPatientDOB(birthdate);
+
 	std::string modality;
-
-	//Get DICOM data
-	//gdcmImageIO->Get...
-
-	std::cout << "Patient name :" << patientname << std::endl;
-	//...
-	std::cout << "Modality :" << modality.c_str() << std::endl;
+	gdcmObject->GetValueFromTag(std::string("0008|0060"), modality);
+	std::string seriesDate;
+	gdcmObject->GetValueFromTag(std::string("0008|0021"), seriesDate);
 
 
-	//Delete allocated pointers
-	delete[] patientname;
-	delete[] patientbirthdate;
-	delete[] patientid;
-	delete[] studydate;
+	std::cout << "Patient name:" << patientName << std::endl;
+	std::cout << "Patient birthdate:" << birthdate << std::endl;
+	std::cout << "Modality:" << modality << std::endl;
+	std::cout << "SeriesDate:" << seriesDate << std::endl;
+
+	delete [] patientName;
+
 	return 0;
 }

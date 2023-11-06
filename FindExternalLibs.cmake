fd0bcd545a7cacf960811d807e49dc973a0bf58e
SIMOES <vincent.simoes@stryker.com>
2023-11-06 15:13:15 +0100

    Correct VTK Ex1

--- FindExternalLibs.cmake
+++ FindExternalLibs.cmake
@@ -52,7 +52,18 @@ if( NOT EXISTS ${VTK_DIR2} )
     message( SEND_ERROR "Cant't find VTK in ${EXTERNAL_LIBS_DIR}" )
 else()
 	set( VTK_DIR "${VTK_DIR2}/lib/cmake/vtk-9.1")
-	find_package( VTK REQUIRED CommonCore CommonDataModel IOGeometry IOXML FiltersSources FiltersGeneral)
+	find_package(VTK COMPONENTS 
+	CommonColor
+	CommonCore
+	CommonDataModel
+	FiltersSources
+	InteractionStyle
+	RenderingContextOpenGL2
+	RenderingCore
+	RenderingFreeType
+	RenderingGL2PSOpenGL2
+	RenderingOpenGL2
+  )
 	#include( ${VTK_USE_FILE} )	
 endif()
 

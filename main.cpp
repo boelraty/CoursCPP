80bed6b5c92a64a7ac5afb52cc766983bfa5d998
SIMOES <vincent.simoes@stryker.com>
2023-11-06 17:05:37 +0100

    VTK Ex9

--- main.cpp
+++ main.cpp
@@ -22,7 +22,25 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	
+	vtkSmartPointer<vtkSTLReader> readerScapula = vtkSmartPointer<vtkSTLReader>::New();
+	readerScapula->SetFileName("C:/Users/vsimoes/ownCloud/CoursISEN/Data/scapula.stl");
+	readerScapula->Update();
+	vtkSmartPointer<vtkSTLReader> readerHumerus = vtkSmartPointer<vtkSTLReader>::New();
+	readerHumerus->SetFileName("C:/Users/vsimoes/ownCloud/CoursISEN/Data/humerus.stl");
+	readerHumerus->Update();
+
+	vtkSmartPointer<vtkPolyDataMapper> mapperScapula = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapperScapula->SetInputData(readerScapula->GetOutput());
+	vtkSmartPointer<vtkPolyDataMapper> mapperHumerus = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapperHumerus->SetInputData(readerHumerus->GetOutput());
+
+	vtkSmartPointer<vtkActor> actorScapula = vtkSmartPointer<vtkActor>::New();
+	actorScapula->SetMapper(mapperScapula);
+	actorScapula->GetProperty()->SetColor(0.87, 0.83, 0.69);
+	vtkSmartPointer<vtkActor> actorHumerus = vtkSmartPointer<vtkActor>::New();
+	actorHumerus->SetMapper(mapperHumerus);
+	actorHumerus->GetProperty()->SetColor(0.87, 0.83, 0.69);
+
 	// Create renderer
 	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
 	// Set background color
@@ -38,7 +56,8 @@ int main(int p_argc, char* p_argv[])
 	interactorWindow->SetRenderWindow(renderWindow);
 
 	// Add actor to renderer
-
+	renderer->AddActor(actorScapula);
+	renderer->AddActor(actorHumerus);
 	// Start rendering
 	renderWindow->Render();
 

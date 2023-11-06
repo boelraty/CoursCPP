4118efb6133ff05f25e006a63f5d1299345645a0
SIMOES <vincent.simoes@stryker.com>
2023-11-06 16:50:00 +0100

    Init VTK Ex9

--- main.cpp
+++ main.cpp
@@ -22,43 +22,11 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	// Create a cube
-	vtkSmartPointer<vtkCubeSource> cubeObject = vtkSmartPointer<vtkCubeSource>::New();
-	cubeObject->SetCenter(0, 0, 0);
-	cubeObject->SetXLength(50);
-	cubeObject->SetYLength(100);
-	cubeObject->SetZLength(200);
-	cubeObject->Update();
-
-	vtkSmartPointer<vtkSTLWriter> writer = vtkSmartPointer<vtkSTLWriter>::New();
-	writer->SetFileName("C:/mesh.stl");
-	writer->SetInputData(cubeObject->GetOutput());
-	writer->Write();
-
-	// Create mapper for the sphere
-	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper->SetInputData(cubeObject->GetOutput());
-
-	// Create actor related to previous mapper
-	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
-	actor->SetMapper(mapper);
-	actor->GetProperty()->SetOpacity(0.4);
-
-	// Create actor to display the axis
-	vtkSmartPointer<vtkAxesActor> axesActor = vtkSmartPointer<vtkAxesActor>::New();
-	axesActor->SetTotalLength(100, 100, 100);
-	//axesActor->AxisLabelsOff();
-
-	vtkSmartPointer<vtkTextActor> textActor = vtkSmartPointer<vtkTextActor>::New();
-	textActor->SetInput("ISEN");
-	textActor->GetTextProperty()->SetColor(1, 0, 1);
-	textActor->GetTextProperty()->SetFontSize(30);
-
+	
 	// Create renderer
 	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-
 	// Set background color
-	renderer->SetBackground(0.5, 0, 1);
+	renderer->SetBackground(1, 1, 1);
 
 	// Create render window
 	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
@@ -70,10 +38,6 @@ int main(int p_argc, char* p_argv[])
 	interactorWindow->SetRenderWindow(renderWindow);
 
 	// Add actor to renderer
-	renderer->AddActor(actor);
-	// Add axesActor to renderer
-	renderer->AddActor(axesActor);
-	renderer->AddActor(textActor);
 
 	// Start rendering
 	renderWindow->Render();

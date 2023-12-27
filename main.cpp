aad6ccf2b695073466984c6fd4585305c988c6d5
SIMOES <vincent.simoes@stryker.com>
2023-12-27 22:10:32 +0100

    Correct Ex final

--- main.cpp
+++ main.cpp
@@ -11,18 +11,30 @@
 #include <vector>
 
 /*---- ITK Includes ----*/
+#include <itkBinaryThresholdImageFilter.h>
 #include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkGDCMImageIO.h>
+#include <itkImageRegionConstIterator.h>
 
 /*---- VTK Includes ----*/
+#include <vtkActor.h>
 #include <vtkImageData.h>
 #include <vtkImageFlip.h>
+#include <vtkMarchingCubes.h>
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
 #include <vtkShortArray.h>
 #include <vtkSmartPointer.h>
 
 //Define image types
 typedef itk::Image<signed short, 2> ShortImageType;
+typedef itk::Image<unsigned char, 2 > UCharImageType;
 
 struct Dimensions
 {
@@ -45,22 +57,70 @@ vtkPolyData* segmentData(const std::vector<ShortImageType::Pointer>& p_inputImag
 	const Dimensions p_dimensions,
 	const Spacing p_spacing)
 {
-	
+	std::vector<UCharImageType::Pointer> outputImages;
 	//Apply threshold on each raw images - ITK Ex6
 	//Same as ITKReader with itk::BinaryThresholdImageFilter
-	for (int z = 0; z < p_inputImages.size(); ++z)
+	for (unsigned int z = 0; z < p_inputImages.size(); ++z) 
 	{
-		//Create thresholder and save outpout in a list if images
+		//Create thresholder and save output in a list if images                         
+		typedef itk::BinaryThresholdImageFilter< ShortImageType, UCharImageType> Thresholder;
+
+		Thresholder::Pointer thresholder = Thresholder::New();
+
+		thresholder->SetLowerThreshold(p_lowerThreshold);
+		thresholder->SetUpperThreshold(p_upperThreshold);
+		thresholder->SetInput(p_inputImages[z]);
+		thresholder->Update(); 
+
+		//Save images in a list
+		outputImages.push_back(thresholder->GetOutput());
 
 	}
 
 	//Build Image Data for input of marching cubes - VTK Ex12 and prior exemples with raw images
+	
+	// Create vtkImageData
+	vtkSmartPointer<vtkImageData> data = vtkSmartPointer<vtkImageData>::New();
+	data->SetDimensions(p_dimensions.x, p_dimensions.y, p_dimensions.z);
+	data->SetSpacing(p_spacing.x, p_spacing.y, p_spacing.z);
 
-	//Apply the marching cubes - VTK Ex12
+	// Fill the image data
+	vtkSmartPointer<vtkUnsignedCharArray> scalars = vtkSmartPointer<vtkUnsignedCharArray>::New();
+	scalars->SetNumberOfValues(p_dimensions.x * p_dimensions.y * p_dimensions.z);
+
+
+	int offset = 0;
+	//Iterate over all images and fill the scalars
+	for (unsigned int z = 0; z < outputImages.size(); ++z)
+	{
+		//Create iterator on current image - ITK Ex3
+		// In the while, set the value of each pixel to the scalars using offset
+		itk::ImageRegionConstIterator<UCharImageType> it
+		(outputImages[z], outputImages[z]->GetLargestPossibleRegion());
+
+		//Initialise iterator to begin
+		it.GoToBegin();
+
+		while (!it.IsAtEnd())//iterateur is at end
+		{
+			scalars->SetValue(offset, it.Get());
+			//increment iterator
+			++it;
+			++offset;
+		}
+	}
+
+	data->GetPointData()->SetScalars(scalars);
+
+	//Create mesh related to image data
+	vtkSmartPointer<vtkMarchingCubes> marchingCubes = vtkSmartPointer<vtkMarchingCubes>::New();
+	marchingCubes->SetInputData(data);
+	marchingCubes->SetValue(0, 1);
+	marchingCubes->Update();
 
 	//Return resulting mesh
 	vtkPolyData* mesh = vtkPolyData::New();
-	//mesh->DeepCopy(marchingcubes->GetOutput());
+	mesh->DeepCopy(marchingCubes->GetOutput());
 
 	return mesh;
 }
@@ -75,7 +135,7 @@ int main(int p_argc, char* p_argv[])
 	std::vector<std::string> listOfFiles;
 	for (const auto& file : std::filesystem::directory_iterator{ inputPath })
 		listOfFiles.push_back(file.path().string());
-
+	std::sort(listOfFiles.begin(), listOfFiles.end());
 	std::cout << "Number of files found: " << listOfFiles.size() << std::endl;
 
 	//Define a list of images to be saved
@@ -84,23 +144,32 @@ int main(int p_argc, char* p_argv[])
 	std::cout << "Read Dicom images in " << inputPath << std::endl;
 	double positionZImage1 = 0., positionZImage2 = 0.;
 	double spacingXY = 0.;
-	for (int i = 0; i < listOfFiles.size(); ++i)
+	for (unsigned int i = 0; i < listOfFiles.size(); ++i)
 	{
-		//try to read the ITK file and save the image - ITK ex4 and ex5
+		//try to read the ITK file and save the image - ITK ex5
 		try
 		{
+			//std::cout << "Read image " << i << " at location: " << listOfFiles[i] << std::endl;
 			//Create ITK reader & GDCMImageIO
+			itk::GDCMImageIO::Pointer gdcmImageIO = itk::GDCMImageIO::New();
 
-			//Save read image in the list
+			//Create the reader to read an image
+			itk::ImageFileReader<ShortImageType>::Pointer reader
+				= itk::ImageFileReader<ShortImageType>::New();
+			reader->SetFileName(listOfFiles[i]);
+			reader->SetImageIO(gdcmImageIO);
+			reader->Update();
 			
+			//Save read image in the list
+			inputImages.push_back(reader->GetOutput());
 
 			//For the first two images, we get the image position (0020, 0032).
 			//From the difference, we compute the spacingZ
-/*			if (i <= 1)
+			if (i <= 1)
 			{
 				std::string currentPosition;
 				gdcmImageIO->GetValueFromTag(std::string("0020|0032"), currentPosition);
-
+				std::cout << "Position image " << i << ": " << currentPosition << std::endl;
 				//image position is at the format "x\y\z", we want to retrieve z position of the 2 first images
 
 				//Split string by \\ and keep last element
@@ -120,11 +189,11 @@ int main(int p_argc, char* p_argv[])
 				//Spacing tag is (0028, 0030)
 				std::string spacingStr;
 				gdcmImageIO->GetValueFromTag(std::string("0028|0030"), spacingStr);
-
+				std::cout << "Spacing field: " << spacingStr << std::endl;
 				//Split string by \\ and keep first element
 				spacingXY = std::stod(spacingStr.substr(0, spacingStr.find(delimiter, 0)));
 			}
-*/
+
 		}
 		//In case of non-image file
 		catch (itk::ExceptionObject& ex)
@@ -137,23 +206,27 @@ int main(int p_argc, char* p_argv[])
 
 	//Get dimension X/Y/Z
 	Dimensions dimensions;
-	//dimensions.x = ...
-	//...
-	//std::cout << "Dimensions: [" << dimensionX << ", " << dimensionY << ", " << dimensionZ << "]" << std::endl;
+	dimensions.x = inputImages[0]->GetLargestPossibleRegion().GetSize()[0];
+	dimensions.y = inputImages[0]->GetLargestPossibleRegion().GetSize()[1];
+	dimensions.z = inputImages.size();
+	std::cout << "Dimensions: [" << dimensions.x << ", " << dimensions.y << ", " << dimensions.z << "]" << std::endl;
 
 	//Get spacing X/Y/Z
-	//...
-	//std::cout << "Spacing: [" << spacingX << ", " << spacingY << ", " << spacingZ << "]" << std::endl;
+	Spacing spacing;
+	spacing.x = spacingXY;
+	spacing.y = spacingXY;
+	spacing.z = fabs(positionZImage1 - positionZImage2);
+	std::cout << "Spacing: [" << spacing.x << ", " << spacing.y << ", " << spacing.z << "]" << std::endl;
 
 
 	//Build the vtkImageData containing the raw volume - VTK Ex12
 	vtkSmartPointer<vtkImageData> rawVTKData = vtkSmartPointer<vtkImageData>::New();
-	//rawVTKData->SetDimensions(...
-	//rawVTKData->SetSpacing(...
+	rawVTKData->SetDimensions(dimensions.x, dimensions.y, dimensions.z);
+	rawVTKData->SetSpacing(spacing.x, spacing.y, spacing.z);
 	
 	//Create scalars to fill the vtkImageData
 	vtkSmartPointer<vtkShortArray> scalars = vtkSmartPointer<vtkShortArray>::New();
-	//scalars->SetNumberOfValues(...
+	scalars->SetNumberOfValues(dimensions.x * dimensions.y * dimensions.z);
 
 	int offset = 0;
 	std::cout << "Apply thresholding on raw images" << std::endl;
@@ -162,47 +235,76 @@ int main(int p_argc, char* p_argv[])
 	{
 		//Create iterator on current image - ITK Ex3
 		// In the while, set the value of each pixel to the scalars using offset
-		
+		itk::ImageRegionConstIterator<ShortImageType> it
+		(inputImages[z], inputImages[z]->GetLargestPossibleRegion());
+
+		//Initialize iterator to beginning
+		it.GoToBegin();
+
+		while (!it.IsAtEnd()) //Iterator IsAtEnd()
+		{
+			scalars->SetValue(offset, it.Get());
+			//Increment iterator
+			++it;
+			++offset;
+		}
 	}
 
 	rawVTKData->GetPointData()->SetScalars(scalars);
 
-	//We need to flip AxisY between ITK and VTK doesn't have the same based referentiel
-	vtkSmartPointer<vtkImageFlip> flipper = vtkSmartPointer<vtkImageFlip>::New();
-	flipper->SetFilteredAxis(1);
-	flipper->SetInputData(rawVTKData);
-	flipper->Update();
-
-	rawVTKData = flipper->GetOutput();
-
 	//Now we want to segment the data
 	std::cout << "Creating scapula mesh" << std::endl;
-	//vtkPolyData* scapula = segmentData(...
+	vtkPolyData* scapula = segmentData(inputImages, 1, 1, dimensions, spacing);
 
 	//Segment from 0 to 1 for Humerus
 	std::cout << "Creating humerus mesh" << std::endl;
-	//vtkPolyData* humerus = segmentData(...
+	vtkPolyData* humerus = segmentData(inputImages, 2, 2, dimensions, spacing);
 
 
 	// ----------- DISPLAY ---------------
 
 	// Create mappers & actors
+	vtkSmartPointer<vtkPolyDataMapper> mapperScapula = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapperScapula->SetInputData(scapula); 
+	mapperScapula->ScalarVisibilityOff();
+
+	vtkSmartPointer<vtkActor> actorScapula = vtkSmartPointer<vtkActor>::New();
+	actorScapula->SetMapper(mapperScapula);
+	actorScapula->GetProperty()->SetColor(0.87, 0.83, 0.69);
+
+	vtkSmartPointer<vtkPolyDataMapper> mapperHumerus = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapperHumerus->SetInputData(humerus);
+	mapperHumerus->ScalarVisibilityOff();
+
+	vtkSmartPointer<vtkActor> actorHumerus = vtkSmartPointer<vtkActor>::New();
+	actorHumerus->SetMapper(mapperHumerus);
+	actorHumerus->GetProperty()->SetColor(0.87, 0.83, 0.69);
 
 	// Create renderer
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
 
 	// Set background color
+	renderer->SetBackground(1, 1, 1);
+
+	// Add actors to renderer
+	renderer->AddActor(actorScapula);
+	renderer->AddActor(actorHumerus);
 
 	// Create render window
-	// 
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	
 	// Associate the renderer to the window
+	renderWindow->AddRenderer(renderer);
 
 	// Create window interactor
-	// 
-	// Add actors to renderer
+	vtkSmartPointer<vtkRenderWindowInteractor> interactorWindow = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactorWindow->SetRenderWindow(renderWindow);
 
 	// Start rendering
-
+	renderWindow->Render();
+	
 	// Start interactor
+	interactorWindow->Start();
 
 	return 0;
 }

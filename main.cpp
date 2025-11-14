6b363eadbfe31646b42d531154245fa33539d56b
SIMOES <vincent.simoes@stryker.com>
2025-11-14 17:01:36 +0100

    Correction ITK Ex8

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple5
+/*!	\brief	Exemple8
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- ITK Includes ----*/
 #include <itkImage.h>
 #include <itkImageFileReader.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkBinaryDilateImageFilter.h>
+#include <itkBinaryErodeImageFilter.h>
+#include <itkGrayscaleFillholeImageFilter.h>
 #include <itkImageFileWriter.h>
-#include <itkGDCMImageIO.h>
-#include <itkBinaryThresholdImageFilter.h>
-#include <itkConnectedComponentImageFilter.h>
-#include <itkRelabelComponentImageFilter.h>
-#include <itkRescaleIntensityImageFilter.h>
 
 /*---- STL Includes ----*/
 #include <string>
@@ -19,79 +18,56 @@
 
 int main(int p_argc, char* p_argv[])
 {
-	//Typedef to define a image type (refer to exercice 1 and replace unsigned char with signed short)
-	typedef itk::Image<short, 2> ShortImageType;
-
-	//Create the GDCM image IO
-	itk::GDCMImageIO::Pointer ioObject = itk::GDCMImageIO::New();
+	//Typedef to define a image type
+	typedef itk::Image<unsigned char, 2> UCharImageType;
 
 	//Create the reader to read an image
-	itk::ImageFileReader<ShortImageType>::Pointer reader 
-		= itk::ImageFileReader<ShortImageType>::New();
-	reader->SetFileName("C:/Users/vsimoes/Downloads/ImageDICOM");
-	reader->SetImageIO(ioObject);
+	itk::ImageFileReader<UCharImageType>::Pointer reader = itk::ImageFileReader<UCharImageType>::New();
+	reader->SetFileName("/home/vsimoes/binaryimage.png");
 	reader->Update();
 
-	try
-	{
-		//#include <itkBinaryThresholdImageFilter.h>
-		typedef itk::Image<unsigned char, 2> UCharImageType;
-		typedef itk::BinaryThresholdImageFilter<ShortImageType, UCharImageType> Thresholder;
+	//Typedef to define a StructuringElement type
+	typedef itk::BinaryBallStructuringElement <unsigned char, 2> StructuringElementType;
 
-		//Create filter to apply threshold on image
-		Thresholder::Pointer thresholder = Thresholder::New();
-		thresholder->SetInput(reader->GetOutput());
-		thresholder->SetLowerThreshold(160);
-		thresholder->SetUpperThreshold(5000);
-		thresholder->SetInsideValue(255);
-		thresholder->SetOutsideValue(0);
-		thresholder->Update();
+	StructuringElementType structuringElement;
+	structuringElement.SetRadius(1);
+	structuringElement.CreateStructuringElement();
 
-//#include <itkConnectedComponentImageFilter.h>
-//#include <itkRelabelComponentImageFilter.h>
-//#include <itkRescaleIntensityImageFilter.h>
-		//Labelize the image
-		typedef itk::Image<int, 2> IntImageType;
-		typedef itk::ConnectedComponentImageFilter<UCharImageType, IntImageType> LabelerType;
-		LabelerType::Pointer labeler = LabelerType::New();
-		labeler->SetInput(thresholder->GetOutput());
-		labeler->Update();
+	typedef itk::Image<unsigned char, 2> UCharImageType;
+	//Typedef to define a DilateFilter type
+	typedef itk::BinaryDilateImageFilter <	UCharImageType,
+		UCharImageType,
+		StructuringElementType> DilateFilterType;
 
-		//Relabelize the image to get info about the group of found pixels
-		typedef itk::RelabelComponentImageFilter<IntImageType, UCharImageType> RelabelerType;
-		RelabelerType::Pointer relabeler = RelabelerType::New();
-		relabeler->SetInput(labeler->GetOutput());
-		relabeler->SetMinimumObjectSize(100);
-		relabeler->Update();
+	//Typedef to define a ErodeFilter type
+	typedef itk::BinaryErodeImageFilter <	UCharImageType,
+		UCharImageType,
+		StructuringElementType> ErodeFilterType;
 
-		//Write the binary image
-		itk::ImageFileWriter<UCharImageType>::Pointer writer
-			= itk::ImageFileWriter<UCharImageType>::New();
-		writer->SetInput(relabeler->GetOutput());
-		writer->SetFileName("image0.png");
-		writer->Write();
+	ErodeFilterType::Pointer eroder = ErodeFilterType::New();
+	eroder->SetKernel(structuringElement);
+	eroder->SetInput(reader->GetOutput());
+	eroder->SetErodeValue(255);
+	eroder->Update();
 
-		std::cout << "Number of found objects :" << relabeler->GetNumberOfObjects() << std::endl;
-		for (int i = 0 ; i < relabeler->GetNumberOfObjects() ; ++i)
-			std::cout << "Area of objects " << i << ":" << relabeler->GetSizeOfObjectInPhysicalUnits(i) << std::endl;
+	DilateFilterType::Pointer dilater = DilateFilterType::New();
+	dilater->SetKernel(structuringElement);
+	dilater->SetInput(eroder->GetOutput());
+	dilater->SetDilateValue(255);
+	dilater->Update();
 
-		//Rescale the image to see colors on labelized output
-		typedef itk::RescaleIntensityImageFilter<UCharImageType, UCharImageType> RescalerType;
-		RescalerType::Pointer rescaler = RescalerType::New();
-		rescaler->SetInput(relabeler->GetOutput());
-		rescaler->Update();
+	//Typedef to define a FillFilter Type
+	typedef itk::GrayscaleFillholeImageFilter < UCharImageType, UCharImageType > FillFilterType;
 
-		//Write the binary image
-		itk::ImageFileWriter<UCharImageType>::Pointer writer2
-			= itk::ImageFileWriter<UCharImageType>::New();
-		writer2->SetInput(rescaler->GetOutput());
-		writer2->SetFileName("image1.png");
-		writer2->Write();
-	
-	}
-	catch (std::exception& ex)
-	{
-		std::cout << ex.what() << std::endl;
-	}
+	FillFilterType::Pointer filler = FillFilterType::New();
+	filler->SetInput(dilater->GetOutput());
+	filler->Update();
+
+	//Write the binary image
+	itk::ImageFileWriter<UCharImageType>::Pointer writer = itk::ImageFileWriter<UCharImageType>::New();
+	writer->SetFileName("/home/vsimoes/resultImage.png");
+	writer->SetInput(filler->GetOutput());
+	writer->Update();
+
 	return 0;
 }

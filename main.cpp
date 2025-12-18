0c6f47f80846603b4ad4ac00e4ab9dfcf00c3362
SIMOES <vincent.simoes@stryker.com>
2025-12-18 17:41:55 +0100

    Correction VTK EX12

--- main.cpp
+++ main.cpp
@@ -1,122 +1,96 @@
 //-------------------------------------------------------------------------------------------------------------------
-/*!	\brief	Exemple1
+/*!	\brief	Exemple12
 *	\file	main.cpp
 *///-----------------------------------------------------------------------------------------------------------------
 
 /*---- VTK Includes ----*/
-#include <vtkSmartPointer.h>
-#include <vtkSphereSource.h>
+#include <vtkActor.h>
+#include <vtkDecimatePro.h>
+#include <vtkImageData.h>
+#include <vtkMarchingCubes.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkActor.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkCamera.h>
-#include <vtkLight.h>
-#include <vtkProperty.h>
-#include <vtkAxesActor.h>
-#include <vtkSTLWriter.h>
-#include <vtkSTLReader.h>
-#include <vtkCutter.h>
-#include <vtkPlane.h>
-#include <vtkClipPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnsignedCharArray.h>
+
 
 int main(int p_argc, char* p_argv[])
 {
-	vtkSmartPointer<vtkSTLReader> reader1 = vtkSmartPointer<vtkSTLReader>::New();
-	reader1->SetFileName("C:/Users/vsimoes/OneDrive - Stryker/Documents/CoursISEN/Data/scapula.stl");
-	reader1->Update();
+	// Create vtkImageData
+	vtkSmartPointer<vtkImageData> data = vtkSmartPointer<vtkImageData>::New();
+	data->SetOrigin(0, 0, 0);
+	data->SetDimensions(100, 100, 100);
+	data->SetSpacing(0.5, 0.5, 0.5);
 
-	vtkSmartPointer<vtkPlane> plane1 = vtkSmartPointer<vtkPlane>::New();
-	plane1->SetNormal(0, 0, 1); // transerve	
-	vtkSmartPointer<vtkPlane> plane2 = vtkSmartPointer<vtkPlane>::New();
-	plane2->SetNormal(1, 1, 0); // coronal
-	vtkSmartPointer<vtkPlane> plane3 = vtkSmartPointer<vtkPlane>::New();
-	plane3->SetNormal(-1, 1, 0); // sagittal
+	// Fill the image data
+	vtkSmartPointer<vtkUnsignedCharArray> scalars = vtkSmartPointer<vtkUnsignedCharArray>::New();
+	scalars->SetNumberOfValues(100 * 100 * 100);
 
-	vtkSmartPointer<vtkClipPolyData> cutter1 = vtkSmartPointer<vtkClipPolyData>::New();
-	cutter1->SetInputData(reader1->GetOutput());
-	cutter1->SetClipFunction(plane1);
-	cutter1->Update();
-	vtkSmartPointer<vtkClipPolyData> cutter2 = vtkSmartPointer<vtkClipPolyData>::New();
-	cutter2->SetInputData(reader1->GetOutput());
-	cutter2->SetClipFunction(plane2);
-	cutter2->Update();
-	vtkSmartPointer<vtkClipPolyData> cutter3 = vtkSmartPointer<vtkClipPolyData>::New();
-	cutter3->SetInputData(reader1->GetOutput());
-	cutter3->GenerateClippedOutputOn();
-	cutter3->SetClipFunction(plane3);
-	cutter3->Update();
-	
-	vtkSmartPointer<vtkPolyDataMapper> mapper1 = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper1->SetInputData(cutter1->GetOutput());
+	for (int i = 0; i < 100; ++i)
+	{
+		for (int j = 0; j < 100; ++j)
+		{
+			for (int k = 0; k < 100; ++k)
+			{
+				if (k > 40 && k < 60 && j > 30 && j < 70 && i > 20 && i < 80)
+					scalars->SetValue(k + j * 100 + i * 100 * 100, 1);
+				else scalars->SetValue(k + j * 100 + i * 100 * 100, 0);
+			}
+		}
+	}
+	data->GetPointData()->SetScalars(scalars);
 
-	vtkSmartPointer<vtkActor> actor1 = vtkSmartPointer<vtkActor>::New();
-	actor1->SetMapper(mapper1);
-	actor1->GetProperty()->SetColor(1, 0, 0);
+	//Create mesh related to image data
+	vtkSmartPointer<vtkMarchingCubes> marchingcubes = vtkSmartPointer<vtkMarchingCubes>::New();
+	marchingcubes->SetInputData(data);
+	marchingcubes->SetValue(0, 1);
+	marchingcubes->Update();
 
-	vtkSmartPointer<vtkPolyDataMapper> mapper2 = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper2->SetInputData(cutter2->GetOutput());
+	std::cout << "Before decimater:" << marchingcubes->GetOutput()->GetNumberOfPoints() << std::endl;
 
-	vtkSmartPointer<vtkActor> actor2 = vtkSmartPointer<vtkActor>::New();
-	actor2->SetMapper(mapper2);
-	actor2->GetProperty()->SetColor(0, 1, 0);
+	vtkSmartPointer<vtkDecimatePro> decimater = vtkSmartPointer<vtkDecimatePro>::New();
+	decimater->SetInputData(marchingcubes->GetOutput());
+	decimater->SetTargetReduction(0.9);
+	decimater->Update();
 
-	vtkSmartPointer<vtkPolyDataMapper> mapper3 = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper3->SetInputData(cutter3->GetClippedOutput());
+	std::cout << "After decimater: " << decimater->GetOutput()->GetNumberOfPoints() << std::endl;
 
-	vtkSmartPointer<vtkActor> actor3 = vtkSmartPointer<vtkActor>::New();
-	actor3->SetMapper(mapper3);
-	actor3->GetProperty()->SetColor(0, 0, 1);
+	// Create mapper for the sphere
+	vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputData(decimater->GetOutput());
 
-	vtkSmartPointer<vtkSTLReader> reader2 = vtkSmartPointer<vtkSTLReader>::New();
-	reader2->SetFileName("C:/Users/vsimoes/OneDrive - Stryker/Documents/CoursISEN/Data/humerus.stl");
-	reader2->Update();
+	// Create actor related to previous mapper
+	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+	actor->GetProperty()->SetColor(0.87, 0.83, 0.69);
 
-	vtkSmartPointer<vtkPolyDataMapper> mapper4 = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper4->SetInputData(reader1->GetOutput());
+	// Create renderer
+	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
 
-	vtkSmartPointer<vtkActor> actor4 = vtkSmartPointer<vtkActor>::New();
-	actor4->SetMapper(mapper4);
-	actor4->GetProperty()->SetColor(0.87, 0.83, 0.69);
-	actor4->GetProperty()->SetOpacity(0.4);
-
-	vtkSmartPointer<vtkPolyDataMapper> mapper5 = vtkSmartPointer<vtkPolyDataMapper>::New();
-	mapper5->SetInputData(reader2->GetOutput());
-
-	vtkSmartPointer<vtkActor> actor5 = vtkSmartPointer<vtkActor>::New();
-	actor5->SetMapper(mapper5);
-	actor5->GetProperty()->SetColor(0.87, 0.83, 0.69);
-
-	// Create renderer - ex2
-	vtkSmartPointer<vtkRenderer> scene = vtkSmartPointer<vtkRenderer>::New();
-	// Set background color - SetBackgroundColor(R[0-1],G[0-1],B[0-1]);
-	scene->SetBackground(1, 1, 1);
-
-	// Add Actor to scene
-	scene->AddActor(actor3);
-	//scene->AddActor(actor2);
-	//scene->AddActor(actor3);
-	scene->AddActor(actor4);
-	scene->AddActor(actor5);
-
-	vtkSmartPointer<vtkAxesActor> axesActor = vtkSmartPointer<vtkAxesActor>::New();
-	axesActor->SetTotalLength(50, 50, 50);
-	scene->AddActor(axesActor);
+	// Set background color
+	renderer->SetBackground(1, 1, 1);
 
 	// Create render window
-	vtkSmartPointer<vtkRenderWindow> window = vtkSmartPointer<vtkRenderWindow>::New();
-	// Associate the renderer to the window - Add renderer method
-	window->AddRenderer(scene);
+	vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+	// Associate the renderer to the window
+	renderWindow->AddRenderer(renderer);
 
-	vtkSmartPointer<vtkRenderWindowInteractor> interactor 
-		= vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	interactor->SetRenderWindow(window);
+	// Create window interactor
+	vtkSmartPointer<vtkRenderWindowInteractor> interactorWindow = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactorWindow->SetRenderWindow(renderWindow);
 
-	// Start rendering - Method Render()
-	window->Render();
-	
-	interactor->Start();
-	
+	// Add actor to renderer
+	renderer->AddActor(actor);
+
+	// Start rendering
+	renderWindow->Render();
+
+	// Start interactor
+	interactorWindow->Start();
+
 	return 0;
-} 
+}

7634c98ceef113e48f723011cf1caa241b334339
SIMOES <vincent.simoes@stryker.com>
2025-12-18 15:31:41 +0100

    Correction VTK EX5

--- main.cpp
+++ main.cpp
@@ -11,18 +11,20 @@
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkCamera.h>
+#include <vtkLight.h>
+#include <vtkProperty.h>
 
 
 int main(int p_argc, char* p_argv[])
 {
-
 	// Create a sphere
 	typedef vtkSmartPointer<vtkSphereSource> Sphere;
 	Sphere sphereObject = Sphere::New();
 	sphereObject->SetCenter(20, 30, 40);
 	sphereObject->SetRadius(50);
-	sphereObject->SetPhiResolution(10);
-	sphereObject->SetThetaResolution(10);
+	sphereObject->SetPhiResolution(100);
+	sphereObject->SetThetaResolution(100);
 	sphereObject->Update();
 
 	// Get bounds of resulting polydata
@@ -39,14 +41,20 @@ int main(int p_argc, char* p_argv[])
 
 	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
 	actor->SetMapper(mapper);
+	actor->GetProperty()->SetColor(1, 1, 1);
 
 	// Create renderer - ex2
 	vtkSmartPointer<vtkRenderer> scene = vtkSmartPointer<vtkRenderer>::New();
 	// Set background color - SetBackgroundColor(R[0-1],G[0-1],B[0-1]);
-	scene->SetBackground(0, 1, 1);
+	scene->SetBackground(0.3, 0.3, 0.3);
 
 	// Add Actor to scene
 	scene->AddActor(actor);
+	
+	vtkSmartPointer<vtkLight> light = vtkSmartPointer<vtkLight>::New();
+	light->SetPosition(100, 0, 0);
+	light->SetIntensity(1);
+	scene->AddLight(light);
 
 	// Create render window
 	vtkSmartPointer<vtkRenderWindow> window = vtkSmartPointer<vtkRenderWindow>::New();
